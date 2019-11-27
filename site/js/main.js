07a0aa536bcf320f5cd7ad46d943a68c37d6319f
West <west2@uleth.ca>
2019-11-27 10:54:39 -0700

    2019/011/27 10:54

--- site/js/main.js
+++ site/js/main.js
@@ -1,5 +1,12 @@
-var pages
-var pageCount
+var pageImg = [
+    
+];
+var pageCount = 0;
+drawPage();
+function drawPage() {
+    $("#site-container").html('<div class="background-image background-image__base"></div><div class="background-image background-image__phase"></div>')
+    
+}
 function nextPage() {
     
 }
